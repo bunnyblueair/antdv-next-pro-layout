3095f2a284627a7719c7a8c72dcb2d46eb0f3b3a
TsMask <340112800@qq.com>
2024-04-27 15:04:02 +0800

    style: 顶部组件命名变更

---
+++ src/components/GlobalHeader/HeaderView.tsx
@@ -0,0 +1,154 @@
+import {
+  defineComponent,
+  computed,
+  toRefs,
+  PropType,
+  ExtractPropTypes,
+} from "vue";
+import type { RouteRecordRaw } from "vue-router";
+import GlobalHeader, { GlobalHeaderProps, globalHeaderProps } from ".";
+import { useRouteContext } from "../../RouteContext";
+import { clearMenuItem } from "../../utils";
+import { LayoutHeader } from "ant-design-vue";
+import PropTypes from "ant-design-vue/es/_util/vue-types";
+import TopNavHeader from "./TopNavHeader";
+import "./HeaderView.css";
+import { CustomRenderProps } from "../../RenderTypings";
+
+export const headerViewProps = {
+  ...globalHeaderProps,
+  headerRender: {
+    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
+    default: () => undefined,
+  },
+  headerTitleRender: {
+    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
+    default: () => undefined,
+  },
+  headerContentRender: {
+    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
+    default: () => undefined,
+  },
+  hasSiderMenu: PropTypes.looseBool,
+  siderWidth: PropTypes.number.def(208),
+};
+
+export type HeaderViewProps = Partial<
+  ExtractPropTypes<typeof headerViewProps> & GlobalHeaderProps
+>;
+
+const HeaderView = defineComponent({
+  name: "HeaderView",
+  inheritAttrs: false,
+  props: headerViewProps,
+  setup(props) {
+    const {
+      prefixCls,
+      isMobile,
+      fixedHeader,
+      hasSiderMenu,
+      headerHeight,
+      layout,
+      onCollapse,
+    } = toRefs(props);
+    const context = useRouteContext();
+    const baseClassName = `${props.prefixCls}-header`;
+    const needFixedHeader = computed(
+      () => fixedHeader.value || context.fixedHeader || layout.value === "mix"
+    );
+    const isMix = computed(() => layout.value === "mix");
+    const isTop = computed(() => layout.value === "top");
+    const needSettingWidth = computed(
+      () =>
+        needFixedHeader.value &&
+        hasSiderMenu.value &&
+        !isTop.value &&
+        !isMobile.value
+    );
+    // cache menu
+    const clearMenuData = computed(
+      () =>
+        (context.menuData &&
+          clearMenuItem(context.menuData as RouteRecordRaw[])) ||
+        []
+    );
+
+    const className = computed(() => {
+      return {
+        [`${baseClassName}`]: true,
+        [`${baseClassName}-${layout.value}`]: true,
+        [`${baseClassName}-fixed`]: needFixedHeader.value,
+      };
+    });
+    const renderContent = () => {
+      let defaultDom = (
+        <GlobalHeader
+          {...props}
+          onCollapse={props.onCollapse}
+          menuData={clearMenuData.value}
+        >
+          {!isMix.value
+            ? props.headerContentRender &&
+              typeof props.headerContentRender === "function"
+              ? props.headerContentRender(props)
+              : props.headerContentRender
+            : null}
+        </GlobalHeader>
+      );
+      if (isTop.value && !isMobile.value) {
+        defaultDom = (
+          <TopNavHeader
+            {...props}
+            mode="horizontal"
+            onCollapse={props.onCollapse}
+            menuData={clearMenuData.value}
+          />
+        );
+      }
+      if (props.headerRender) {
+        return props.headerRender(props);
+      }
+      return defaultDom;
+    };
+
+    /**
+     * 计算侧边栏的宽度，不然导致左边的样式会出问题
+     */
+    const width = computed(() => {
+      return layout.value !== "mix" && needSettingWidth.value
+        ? `calc(100% - ${props.collapsed ? 48 : props.siderWidth}px)`
+        : "100%";
+    });
+    const right = computed(() => (needFixedHeader.value ? 0 : undefined));
+    return () => {
+      return (
+        <>
+          {needFixedHeader.value && (
+            <LayoutHeader
+              style={{
+                height: `${headerHeight.value}px`,
+                lineHeight: `${headerHeight.value}px`,
+                background: "transparent",
+              }}
+            />
+          )}
+          <LayoutHeader
+            style={{
+              padding: 0,
+              height: `${headerHeight.value}px`,
+              lineHeight: `${headerHeight.value}px`,
+              width: width.value,
+              zIndex: layout.value === "mix" ? 100 : 19,
+              right: right.value,
+            }}
+            class={className.value}
+          >
+            {renderContent()}
+          </LayoutHeader>
+        </>
+      );
+    };
+  },
+});
+
+export default HeaderView;
--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,157 +1,117 @@
-import {
-  defineComponent,
-  computed,
-  toRefs,
-  PropType,
-  ExtractPropTypes,
-} from "vue";
+import { computed, type FunctionalComponent, type ExtractPropTypes } from "vue";
 import type { RouteRecordRaw } from "vue-router";
-import GlobalHeader, {
-  globalHeaderProps,
-  type GlobalHeaderProps,
-} from "./GlobalHeader";
-import { useRouteContext } from "../../RouteContext";
-import { clearMenuItem } from "../../utils";
-import { LayoutHeader } from "ant-design-vue";
-import PropTypes from "ant-design-vue/es/_util/vue-types";
+import {
+  defaultRenderLogoAndTitle,
+  siderMenuProps,
+} from "../SiderMenu/SiderMenu";
 import TopNavHeader from "./TopNavHeader";
-import "./index.css";
-import { CustomRenderProps } from "src/RenderTypings";
+import { clearMenuItem } from "../../utils";
 
-export const headerViewProps = {
-  ...globalHeaderProps,
-  headerRender: {
-    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
+import "./index.css";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons-vue";
+
+import type { PropType } from "vue";
+import type { MenuDataItem } from "../../typings";
+import { defaultSettingProps } from "../../defaultSettings";
+import PropTypes from "ant-design-vue/es/_util/vue-types";
+import type { CustomRenderProps, MenuContentRender } from "../../RenderTypings";
+
+export const globalHeaderProps = {
+  ...defaultSettingProps,
+  prefixCls: PropTypes.string.def("ant-pro"),
+  collapsed: PropTypes.looseBool,
+  isMobile: PropTypes.looseBool,
+  logo: siderMenuProps.logo,
+  logoStyle: siderMenuProps.logoStyle,
+  menuData: {
+    type: Array as PropType<MenuDataItem[]>,
+    default: () => [],
+  },
+  splitMenus: siderMenuProps.splitMenus,
+  menuRender: {
+    type: [Object, Function] as PropType<MenuContentRender>,
     default: () => undefined,
   },
-  headerTitleRender: {
-    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
+  menuHeaderRender: siderMenuProps.menuHeaderRender,
+  menuItemRender: siderMenuProps.menuItemRender,
+  subMenuItemRender: siderMenuProps.subMenuItemRender,
+  rightContentRender: {
+    type: [Object, Function] as PropType<CustomRenderProps>,
     default: () => undefined,
   },
-  headerContentRender: {
-    type: [Object, Function, Boolean] as PropType<CustomRenderProps>,
-    default: () => undefined,
-  },
-  hasSiderMenu: PropTypes.looseBool,
-  siderWidth: PropTypes.number.def(208),
+  collapsedButtonRender: siderMenuProps.collapsedButtonRender,
+  matchMenuKeys: siderMenuProps.matchMenuKeys,
+
+  // events
+  onMenuHeaderClick: PropTypes.func,
+  onCollapse: siderMenuProps.onCollapse,
+  onOpenKeys: siderMenuProps.onOpenKeys,
+  onSelect: siderMenuProps.onSelect,
 };
 
-export type HeaderViewProps = Partial<
-  ExtractPropTypes<typeof headerViewProps> & GlobalHeaderProps
+export type GlobalHeaderProps = Partial<
+  ExtractPropTypes<typeof globalHeaderProps>
 >;
 
-const HeaderView = defineComponent({
-  name: "HeaderView",
-  inheritAttrs: false,
-  props: headerViewProps,
-  setup(props) {
-    const {
-      prefixCls,
-      isMobile,
-      fixedHeader,
-      hasSiderMenu,
-      headerHeight,
-      layout,
-      onCollapse,
-    } = toRefs(props);
-    const context = useRouteContext();
-    const baseClassName = `${props.prefixCls}-header`;
-    const needFixedHeader = computed(
-      () => fixedHeader.value || context.fixedHeader || layout.value === "mix"
-    );
-    const isMix = computed(() => layout.value === "mix");
-    const isTop = computed(() => layout.value === "top");
-    const needSettingWidth = computed(
-      () =>
-        needFixedHeader.value &&
-        hasSiderMenu.value &&
-        !isTop.value &&
-        !isMobile.value
-    );
-    // cache menu
-    const clearMenuData = computed(
-      () =>
-        (context.menuData &&
-          clearMenuItem(context.menuData as RouteRecordRaw[])) ||
-        []
-    );
+const GlobalHeader: FunctionalComponent<GlobalHeaderProps> = (
+  props,
+  { slots, emit }
+) => {
+  const {
+    isMobile,
+    rightContentRender,
+    onMenuHeaderClick,
+    layout,
+    splitMenus,
+    menuData,
+  } = props;
+  const baseClassName = `${props.prefixCls}-global-header`;
 
-    const className = computed(() => {
-      return {
-        [`${baseClassName}`]: true,
-        [`${baseClassName}-${layout.value}`]: true,
-        [`${baseClassName}-fixed`]: needFixedHeader.value,
-      };
-    });
-    const renderContent = () => {
-      let defaultDom = (
-        <GlobalHeader
-          {...props}
-          onCollapse={props.onCollapse}
-          menuData={clearMenuData.value}
-        >
-          {!isMix.value
-            ? props.headerContentRender &&
-              typeof props.headerContentRender === "function"
-              ? props.headerContentRender(props)
-              : props.headerContentRender
-            : null}
-        </GlobalHeader>
-      );
-      if (isTop.value && !isMobile.value) {
-        defaultDom = (
-          <TopNavHeader
-            {...props}
-            mode="horizontal"
-            onCollapse={props.onCollapse}
-            menuData={clearMenuData.value}
-          />
-        );
-      }
-      if (props.headerRender) {
-        return props.headerRender(props);
-      }
-      return defaultDom;
-    };
+  if (layout === "mix" && !isMobile && splitMenus) {
+    const noChildrenMenuData = (menuData || []).map((item: any) => ({
+      ...item,
+      children: undefined,
+    })) as RouteRecordRaw[];
+    const clearMenuData = clearMenuItem(noChildrenMenuData);
+    return (
+      <TopNavHeader
+        {...props}
+        mode="horizontal"
+        splitMenus={false}
+        menuData={clearMenuData}
+      />
+    );
+  }
 
-    /**
-     * 计算侧边栏的宽度，不然导致左边的样式会出问题
-     */
-    const width = computed(() => {
-      return layout.value !== "mix" && needSettingWidth.value
-        ? `calc(100% - ${props.collapsed ? 48 : props.siderWidth}px)`
-        : "100%";
-    });
-    const right = computed(() => (needFixedHeader.value ? 0 : undefined));
-    return () => {
-      return (
+  const onCollapse = () => {
+    emit("collapse", !props.collapsed);
+  };
+
+  return (
+    <div class={baseClassName}>
+      {isMobile && (
+        <span onClick={onCollapse}>
+          {props.collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        </span>
+      )}
+      {layout === "mix" && !isMobile && (
         <>
-          {needFixedHeader.value && (
-            <LayoutHeader
-              style={{
-                height: `${headerHeight.value}px`,
-                lineHeight: `${headerHeight.value}px`,
-                background: "transparent",
-              }}
-            />
-          )}
-          <LayoutHeader
-            style={{
-              padding: 0,
-              height: `${headerHeight.value}px`,
-              lineHeight: `${headerHeight.value}px`,
-              width: width.value,
-              zIndex: layout.value === "mix" ? 100 : 19,
-              right: right.value,
-            }}
-            class={className.value}
-          >
-            {renderContent()}
-          </LayoutHeader>
+          <div class={`${baseClassName}-logo`} onClick={onMenuHeaderClick}>
+            {defaultRenderLogoAndTitle(
+              { ...props, collapsed: false },
+              "headerTitleRender"
+            )}
+          </div>
         </>
-      );
-    };
-  },
-});
+      )}
+      <div class={`${baseClassName}-flex`}>{slots.default?.()}</div>
+      {rightContentRender && typeof rightContentRender === "function"
+        ? rightContentRender(props)
+        : rightContentRender}
+    </div>
+  );
+};
+GlobalHeader.inheritAttrs = false;
+GlobalHeader.emits = ["menuHeaderClick", "collapse", "openKeys", "select"];
 
-export default HeaderView;
+export default GlobalHeader;
