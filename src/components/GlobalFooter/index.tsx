2d8b00506d4df8d35b74d65eccc59519cd697f9e
TsMask <340112800@qq.com>
2024-04-27 13:48:41 +0800

    fix: 底脚组件样式修复

--- src/components/GlobalFooter/index.tsx
+++ src/components/GlobalFooter/index.tsx
@@ -1,65 +1,69 @@
-import './index.css';
+import { defineComponent, SetupContext, PropType, ExtractPropTypes } from "vue";
+import { CustomRenderFalse } from "src/RenderTypings";
+import "./index.css";
 
-import { defineComponent, type PropType, type SetupContext } from 'vue';
-import type { WithFalse, CustomRender } from '../../typings';
+export const globalFooterProps = {
+  links: {
+    type: Array as PropType<
+      {
+        key?: string;
+        title: string;
+        href: string;
+        blankTarget?: boolean;
+      }[]
+    >,
+    required: true,
+  },
+  copyright: {
+    type: [Object, Function, String, Boolean] as PropType<CustomRenderFalse>,
+    default: () => undefined,
+  },
+  prefixCls: {
+    type: String,
+    default: "ant-pro",
+  },
+};
 
-export type Links = WithFalse<
-  {
-    key?: string;
-    title: CustomRender;
-    href: string;
-    blankTarget?: boolean;
-  }[]
+export type GlobalFooterProps = Partial<
+  ExtractPropTypes<typeof globalFooterProps>
 >;
 
-export interface GlobalFooterProps {
-  links?: Links;
-  copyright?: CustomRender;
-  prefixCls?: string;
-}
-
 export default defineComponent({
-  name: 'GlobalFooter',
-  props: {
-    links: {
-      type: [Array, Boolean] as PropType<Links>,
-      required: true,
-    },
-    copyright: {
-      type: [Object, Function, String, Boolean] as PropType<WithFalse<CustomRender>>,
-      default: () => undefined,
-    },
-    prefixCls: {
-      type: String,
-      default: 'ant-pro',
-    },
-  },
-  setup(props: GlobalFooterProps, { slots }: SetupContext) {
+  name: "GlobalFooter",
+  props: globalFooterProps,
+  setup(props, { slots }) {
+    const baseClassName = `${props.prefixCls}-global-footer`;
+    const copyright = props.copyright || (slots.copyright && slots.copyright());
+
     if (
-      (props.links == null || props.links === false || (Array.isArray(props.links) && props.links.length === 0)) &&
+      Array.isArray(props.links) &&
+      props.links.length === 0 &&
       (props.copyright == null || props.copyright === false)
     ) {
       return null;
     }
 
-    return () => {
-      const baseClassName = `${props.prefixCls}-global-footer`;
-      const copyright = props.copyright || (slots.copyright && slots.copyright());
+    return () => (
+      <footer class={baseClassName}>
+        {props.links && (
+          <div class={`${baseClassName}-links`}>
+            {props.links.map((link) => (
+              <a
+                key={link.key}
+                title={link.title}
+                target={link.blankTarget ? "_blank" : "_self"}
+                href={link.href}
+              >
+                {link.title}
+              </a>
+            ))}
+          </div>
+        )}
 
-      return (
-        <footer class={baseClassName}>
-          {props.links && (
-            <div class={`${baseClassName}-links`}>
-              {props.links.map((link) => (
-                <a key={link.key} title={link.key} target={link.blankTarget ? '_blank' : '_self'} href={link.href}>
-                  {link.title}
-                </a>
-              ))}
-            </div>
-          )}
-          {props.copyright && <div class={`${baseClassName}-copyright`}>{copyright}</div>}
-        </footer>
-      );
-    };
+        {props.copyright && (
+          <div class={`${baseClassName}-copyright`}>{copyright}</div>
+        )}
+      </footer>
+    );
   },
 });
