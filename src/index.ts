d6c8e0da458eece0b7e240192c908d114cc5b10a
TsMask <340112800@qq.com>
2024-05-13 22:55:53 +0800

    feat: 导出组件内hooks函数

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 export * from "./RouteContext";
 export * from "./typings";
+export * from "./hooks/useContext";
+export * from "./hooks/useMediaScreen";
 export * from "./utils/getMenuData";
 export * from "./utils/mediaTheme";
-export { createContext, useContext } from "./hooks/context";
-export type { ContextType, CreateContext } from "./hooks/context";
 
-export { default as SiderMenuWrapper } from "./components/SiderMenu";
-export {
-  default as BaseMenu,
-  baseMenuProps,
-} from "./components/SiderMenu/BaseMenu";
 export type { BaseMenuProps } from "./components/SiderMenu/BaseMenu";
+export { default as BaseMenu } from "./components/SiderMenu/BaseMenu";
 
 export type { GlobalHeaderProps } from "./components/GlobalHeader";
 export { default as GlobalHeader } from "./components/GlobalHeader";
